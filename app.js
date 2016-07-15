48bc906933948e7353e810b702f838dae1a71a1c
Fabio Rocha <fabio@fabiofarias.com.br>
2016-07-15 11:36:20 -0300

    Busca cep

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 new Vue({
-    el:"#vue",
+    el:"body",
+    data: {
+        cep:'',
+        endereco: {},
+        naoLocalizado: false
+    },
     methods:{
         buscar: function ()
         {
-            window.console.log('HELLO');
+            var self = this;
+            if(/^[0-9]{8}$/.test(this.cep)){
+                jQuery.getJSON('http://viacep.com.br/ws/'+this.cep+'/json/',function (endereco) {
+                    self.endereco = endereco;
+                    jQuery(self.numero).focus();
+                });
+            }
         }
     }
 });
